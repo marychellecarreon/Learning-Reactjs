0574b32919fd6b4156799552e82b9ae6c05a2d96
Mary Chelle Carreon <marychellecarreon@gmail.com>
2017-09-12 05:34:04 +0800

    first commit

---
+++ src/Components/ProjectItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProjectItem extends Component {
+  render() {
+    return (
+      <li className="Projects">
+        My Projects
+      </li>
+    );
+  }
+}
+
+export default ProjectItem;
---
+++ src/Components/Projects.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  
+  render() {
+    let projectItems;
+    if(this.props.projects){
+      projectItems = this.props.projects.map(project => {
+        console.log(project);
+      });
+    }
+    return (
+      <div className="Projects">
+        My Projects
+      </div>
+    );
+  }
+}
+
+export default Projects;
